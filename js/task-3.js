81d64b43b189c7ec0d92bbef820252f597e3c02d
Milaya78 <ela_keyt@mail.ru>
2024-06-07 09:50:25 +0200

    js

--- js/task-3.js
+++ js/task-3.js
@@ -2,7 +2,8 @@ function getElementWidth(content, padding, border) {
   const contentNumber = Number.parseFloat(content);
   const paddingNumber = Number.parseFloat(padding);
   const borderNumber = Number.parseFloat(border);
-  return `${contentNumber + paddingNumber * 2 + borderNumber * 2}`;
+  const result = contentNumber + paddingNumber * 2 + borderNumber * 2;
+  return result;
 }
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
